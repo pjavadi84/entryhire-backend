59bfb7f0b8709748d0e0ae3c6dccfa4071e03882
pjavadi84 <pouya.javadi.resume@gmail.com>
2021-03-21 19:36:39 -0700

    modified session store to figure out if the session runs on local senv or deployed env

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: "_entryhire", domain: "THENAMEOFYOURDOMAIN"
+if Rails.env == "production"
+    Rails.application.config.session_store :cookie_store, key: "_entryhire", domain: "THENAMEOFYOURDOMAIN"
+else
+    Rails.application.config.session_store :cookie_store, key: "_entryhire"
+end
