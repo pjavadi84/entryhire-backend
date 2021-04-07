f1b31373ee7b157e6a6cfc2d9874026302673547
pjavadi84 <pouya.javadi.resume@gmail.com>
2021-04-07 01:00:50 -0700

    removed null parameters from create method

--- app/controllers/v1/registrations_controller.rb
+++ app/controllers/v1/registrations_controller.rb
@@ -1,13 +1,18 @@
 # module V1 
     class V1::RegistrationsController < ApplicationController
         def create 
-            binding.pry
+            # binding.pry
             partner = Partner.create!(
+                # company_name: params['partner']['company_name']
+                # contact_name: params['partner']['contact_name']
+                # zipcode: params['partner']['zipcode']
+                # website: params['partner']['website']
+                # about_us: params['partner']['about_us']
                 email: params['partner']['email'], 
                 password: params['partner']['password'],
                 password_confirmation: params['partner']['password_confirmation']
             )
-
+            binding.pry
             if partner
                 session[:partner_id] = partner.id
                 render json: {
@@ -18,6 +23,8 @@
                 render json: { status: 500 }
             end
         end
+
+
 
     end
 # end
